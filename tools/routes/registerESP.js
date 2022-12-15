9b83fe9034cb80149fd46c03b6f57e6772ed48ae
SergioLucasINT <99209004+SergioLucasINT@users.noreply.github.com>
2022-12-15 19:26:52 -0300

    Update registerESP.js

--- tools/routes/registerESP.js
+++ tools/routes/registerESP.js
@@ -1,79 +1,88 @@
-const express = require('express'); 
+const express = require("express");
 const app = express();
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 const router = express.Router();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 let MACAddress = [];
 
-let beacondata = {};
+// Action 0 é registro Inicial
+// Action 1 é esperar por instruções
 
-const DBPATH = 'dbUser.db';
-
-// Functions
-const functions = require('../functions/crud');
-
-var query_data = {
-    table: '`beacons`',
-    create_columns: '`Reg_Area`, `Name`, `Mac_Add`'
+let dados = {
+  action: "0",
 };
 
-app.use(express.json()); 
-app.use(express.static('public'));
+const DBPATH = "dbUser.db";
+
+// Functions
+const functions = require("../functions/crud");
+
+var query_data = {
+  table: "`beacons`",
+  create_columns: "`Reg_Area`, `Name`, `Mac_Add`",
+};
+
+app.use(express.json());
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-router.post('/beacon', (req, res) => {
+router.post("/beacon", (req, res) => {
+  texto = req.body;
+  console.log(texto);
+  console.log("Recebi um dado");
+  res.send(texto);
 
-    texto = req.body;
-    console.log(texto);
-    console.log("Recebi um dado");
-    res.send(texto);
-    
-    var db = new sqlite3.Database(DBPATH);
-  
-    db.all(functions.readNode(query_data['table'], '*'), [],  async (err, beacons ) => {
+  var db = new sqlite3.Database(DBPATH);
 
-        if (err) {
-            throw err;
-        }
-        console.log(beacons);
+  db.all(
+    functions.readNode(query_data["table"], "*"),
+    [],
+    async (err, beacons) => {
+      if (err) {
+        throw err;
+      }
 
-        for (let i = 0; i < beacons.length; i++) {
-            MACAddress.push(beacons[i].Mac_Add);
-            console.log(MACAddress);
-        }
-
-        console.log(MACAddress);
-        console.log(texto.MACAddress);
-        
+      for (let i = 0; i < beacons.length; i++) {
         if (MACAddress.includes(texto.MACAddress)) {
-            console.log("MACAddress já existe");
-            MACAddress = [];
         } else {
-            console.log("MACAddress não existe");
-            MACAddress = [];
-            db.run(functions.createNode(query_data['table'], query_data['create_columns'], 0  + ", '" + texto.name + "', '" + texto.MACAddress + "'")), [],  async (err, beacons ) => {
-
-                if (err) {
-                    throw err;
-                }
-                console.log("MACAddress adicionado");
-            };	
+          MACAddress.push(beacons[i].Mac_Add);
         }
-    });
+      }
 
-    console.log(MACAddress);
+      if (MACAddress.includes(texto.MACAddress)) {
+        console.log("MACAddress já existe");
+        dados[texto.MACAddress] = 1;
+      } else {
+        console.log("MACAddress não existe");
+        db.run(
+          functions.createNode(
+            query_data["table"],
+            query_data["create_columns"],
+            0 + ", '" + texto.name + "', '" + texto.MACAddress + "'"
+          )
+        ),
+          [],
+          async (err, beacons) => {
+            if (err) {
+              throw err;
+            }
+            console.log("MACAddress adicionado");
+            dados[texto.MACAddress] = 1;
+          };
+      }
+    }
+  );
 
-    db.close();
+  db.close();
 });
 
-router.get('/beacon', (req, res) => {
-res.header("Access-Control-Allow-Origin", "*");
+router.get("/beacon", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
   console.log("Recebi a requisição de dados");
   json = JSON.stringify(dados);
   res.send(json);
 });
-
 
 module.exports = router;
