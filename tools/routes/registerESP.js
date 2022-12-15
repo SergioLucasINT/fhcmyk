a76344876e4fde9290ff7549536bd84acc95712c
SergioLucasINT <99209004+SergioLucasINT@users.noreply.github.com>
2022-12-15 15:42:50 -0300

    Update registerESP.js

--- tools/routes/registerESP.js
+++ tools/routes/registerESP.js
@@ -35,6 +35,7 @@ router.post('/beacon', (req, res) => {
           if (err) {
               throw err;
           }
+          console.log("Aqui");
           console.log(beacons);
           for (i in beacons) {
             MACAddress.push(beacons[i].MACAddress);
